3fe252af1d1b99c3f8ab5ea49cc936ace8bab8a9
Victoria Brauer <torichip4@gmail.com>
2025-03-20 17:01:52 +0400

    deleted one button

---
+++ keyboards_box/quiz_kb.py
@@ -0,0 +1,19 @@
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+def get_topic_keyboard():
+    """Создает кнопки с темами"""
+    buttons = [
+        [InlineKeyboardButton(text="История", callback_data="quiz_history")],
+        [InlineKeyboardButton(text="Наука", callback_data="quiz_science")],
+        [InlineKeyboardButton(text="География", callback_data="quiz_geography")],
+        [InlineKeyboardButton(text="Литература", callback_data="quiz_literature")]
+    ]
+    return InlineKeyboardMarkup(inline_keyboard=buttons)
+
+def get_quiz_buttons():
+    """Кнопки для продолжения квиза"""
+    buttons = [
+        [InlineKeyboardButton(text="🎭 Сменить тему", callback_data="change_topic")],
+        [InlineKeyboardButton(text="🏁 Закончить квиз", callback_data="end_quiz")]
+    ]
+    return InlineKeyboardMarkup(inline_keyboard=buttons)
