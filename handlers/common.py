960480f995b90203499c10d346c618005a382ebb
Victoria Brauer <torichip4@gmail.com>
2025-03-20 15:47:21 +0400

    Add new command stop

--- handlers/common.py
+++ handlers/common.py
@@ -7,14 +7,9 @@ router = Router()
 
 @router.message(Command("start"))
 async def cmd_start(message: types.Message):
-    await message.answer(f"Привет, {message.chat.first_name}! Выбери действие!", reply_markup=kb1)
+    await message.answer(f"Привет, {message.chat.first_name}! Выберите действие!\U0001F338", reply_markup=kb1)
 
 
-
-# @router.message(F.text & ~F.text.startswith("/gpt"))  # Исключаем "/gpt"
-# async def echo(message: types.Message):
-#     print(message.text)
-#     if "stop" in message.text:
-#         await message.answer("Спасибо за использование бота! Введите /start, если хотите начать снова.")
-#     else:
-#         await message.answer(message.text)
+@router.message(Command("stop"))
+async def cmd_start(message: types.Message):
+    await message.answer(f"До новых встреч, {message.chat.first_name}!Спасибо за использование бота! Введи /start, если хочешь начать снова.\U0001F338", reply_markup=kb1)
